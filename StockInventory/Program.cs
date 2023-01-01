b250215f0aa0c3a36a7e3d8607c755a00bc56c63
Rutuja412 <rutujakadam4545@gmail.com>
2023-01-01 23:29:36 +0530

    [Rutuja]:Adding Sell & Buy

---
+++ StockInventory/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StockInventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StockAccountManagement stockAccountManagement = new StockAccountManagement();
+
+            string stockFilePath = @"E:\RFP 232 BATCH\JSON\StockInventory\Stock.json";
+            stockAccountManagement.ReadStockJsonFile(stockFilePath);
+            string customerFilePath = @"E:\RFP 232 BATCH\JSON\StockInventory\Customer.json";
+            stockAccountManagement.ReadCustomerJsonFile(customerFilePath);
+            stockAccountManagement.BuyStock("Google");
+            stockAccountManagement.SellStock("Google");
+            stockAccountManagement.WriteToStockJsonFile(stockFilePath);
+            stockAccountManagement.WriteToStockJsonFile(customerFilePath);
+        }
+    }
+}
---
+++ StockInventory/StockAccountManagement.cs
@@ -0,0 +1,108 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace StockInventory
+{
+    public class StockAccountManagement
+    {
+        double amount = 1000;
+        List<StockDetails> stock = new List<StockDetails>();
+        List<StockDetails> customer = new List<StockDetails>();
+        public void ReadStockJsonFile(string filePath)
+        {
+            var json = File.ReadAllText(filePath);
+            this.stock = JsonConvert.DeserializeObject<List<StockDetails>>(json);
+            Console.WriteLine("StockName  StockPrice  NoOfShares");
+            foreach (var content in stock)
+            {
+                Console.WriteLine(content.StockName + "   " + content.StockPrice + "   " + content.NoOfShares);
+            }
+        }
+        public void ReadCustomerJsonFile(string filePath)
+        {
+            var json = File.ReadAllText(filePath);
+            this.customer = JsonConvert.DeserializeObject<List<StockDetails>>(json);
+            Console.WriteLine("\nStockName  StockPrice  NoOfShares");
+            foreach (var content in customer)
+            {
+                Console.WriteLine(content.StockName + "   " + content.StockPrice + "   " + content.NoOfShares);
+            }
+        }
+        public void BuyStock(string name)
+        {
+            foreach (var data in stock)
+            {
+                int count = 0;
+                if (data.StockName.Equals(name))
+                {
+                    Console.WriteLine("Enter required stocks you need to buy");
+                    int noOfStocks = Convert.ToInt32(Console.ReadLine());
+                    if (noOfStocks * data.StockPrice <= amount && noOfStocks <= data.NoOfShares)
+                    {
+                        StockDetails details = new StockDetails()
+                        {
+                            StockName = data.StockName,
+                            StockPrice = data.StockPrice,
+                            NoOfShares = noOfStocks
+                        };
+                        data.NoOfShares -= noOfStocks;
+                        amount -= data.StockPrice * noOfStocks;
+                        foreach (var account in customer)
+                        {
+                            if (account.StockName.Equals(name))
+                            {
+                                count++;
+                            }
+                        }
+                        if (count == 1)
+                        {
+                            data.NoOfShares += noOfStocks;
+                        }
+                        else
+                        {
+                            customer.Add(details);
+                        }
+                    }
+                }
+            }
+        }
+        public void SellStock(string name)
+        {
+            foreach (var data in customer)
+            {
+                if (data.StockName.Equals(name))
+                {
+                    Console.WriteLine("Enter number stocks you want to sell");
+                    int noOfStocks = Convert.ToInt32(Console.ReadLine());
+                    if (noOfStocks <= data.NoOfShares)
+                    {
+                        data.NoOfShares -= noOfStocks;
+                        amount += data.StockPrice * noOfStocks;
+                        foreach (var account in stock)
+                        {
+                            if (account.StockName.Equals(name))
+                            {
+                                data.NoOfShares += noOfStocks;
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public void WriteToStockJsonFile(string filePath)
+        {
+            var json = JsonConvert.SerializeObject(stock);
+            File.WriteAllText(filePath, json);
+        }
+        public void WriteToCustomerJsonFile(string filePath)
+        {
+            var json = JsonConvert.SerializeObject(customer);
+            File.WriteAllText(filePath, json);
+        }
+    }
+}
